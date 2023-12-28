5966baafebc0f539c59ef29abd89724ef1c3ece2
Kim ju hwan <minecraft_bos@naver.com>
2023-12-28 10:07:48 +0900

    🧑‍💻:: Baekjoon 1330
    백준 1330번 문제

---
+++ 1330.swift
@@ -0,0 +1,12 @@
+let input = readLine()!
+let lineArr = input.split(separator: " ")
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+
+if a > b {
+    print(">")
+} else if a < b {
+    print("<")
+} else {
+    print("==")
+}
